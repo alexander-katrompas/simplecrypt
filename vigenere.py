36770ca2d57a023f31637e7d701ca401d3de5fba
alexander-katrompas <alexander.katrompas@austincc.edu>
2020-02-02 13:41:34 -0600

    added freq_score(text) added find_key(ctext, keylen) added crack(message)

--- vigenere.py
+++ vigenere.py
@@ -7,7 +7,51 @@ Author: Alex Katrompas
 import caesar
 from collections import Counter
 import constants as cs
+import operator
 import re
+
+def freq_score(text):
+    freqs = {
+        'a': 8167,
+        'b': 1492,
+        'c': 2782,
+        'd': 4253,
+        'e': 12702,
+        'f': 2228,
+        'g': 2015,
+        'h': 6094,
+        'i': 6966,
+        'j': 153,
+        'k': 772,
+        'l': 4025,
+        'm': 2406,
+        'n': 6749,
+        'o': 7507,
+        'p': 1929,
+        'q': 95,
+        'r': 5987,
+        's': 6327,
+        't': 9056,
+        'u': 2758,
+        'v': 978,
+        'w': 2360,
+        'x': 150,
+        'y': 1974,
+        'z': 74
+    }
+
+    score = 0
+    for c in text:
+        if c == ' ':
+            score += 10000
+        elif c.lower() in freqs:
+            score += freqs[c.lower()]
+        elif ord(c) >= 128:
+            score -= 5000
+        else:
+            score -= 1000
+
+    return score
 
 def generateKeyString(key, length):
     keyString = ""
@@ -67,9 +111,9 @@ def partition(text, num):
         # i % num will be 0 to max key length
         cols[i % num] += c
     return cols # example, ['ace','bdf']
-    # the reason for partitioning by key length is that each partition
-    # will have been partioned into what was encrypted by the same key
-    # therefore the IC should be close to the English IC and not random
+    # the reason for partitioning by *possible* key length is that each
+    # partition will have been partioned into what was encrypted by the same
+    # key therefore the IC should be close to the English IC and not random
 
 def find_keylen_ics(ctext):
     low = 2 # smallest key length
@@ -87,18 +131,42 @@ def find_keylen_ics(ctext):
 
     return sorted(results.items(), key=lambda kv: -kv[1])
 
+def find_key(ctext, keylen):
+    # this assumes key is alpha only
+    ctext = ctext.lower()
+    key = ""
+
+    # break up text so that each partition is what is encrpted by same key position
+    parted = partition(ctext, keylen)
+
+    # for each block that could be dectrypted by the key length given
+    for col in parted: 
+        # print(col)
+        scores = {}
+        for i, letter in enumerate(cs.ALPHALOWER): # enumerating 1:A, 2:B, etc
+            # transposed will be each group of characters encrypted with the
+            # same letter. if key len is 4, there will be 4 groups of 26 where
+            # each group represents all possible characters as that group's key
+            transposed = [cs.ALPHALOWER[(cs.ALPHALOWER.index(c) - i) %
+                len(cs.ALPHALOWER)] for c in col]
+            # scores is ranks each of letter a-z, for each group
+            scores[letter] = freq_score(transposed)
+        key += max(scores.items(), key=operator.itemgetter(1))[0]
+
+    return key
+
 def crack(message):
+    # note this version of cracking does not allow for non alpha characters
+   
     message = message.lower()
-    message = "VIAYISBEQOWPKMQYRQAYCVEPKMQYRLOGMXRYSXNYMLDSLVIQORRORKSPJOGFYWCC"
-    print("   =========================")
+    print("   ================================")
     best = find_keylen_ics(message)
     for k, v in best:
-        print("%14s%3d%5s%6.3f" % ("key length:", k,"ic:", v))
-    print("   =========================\n")
-    
-    
-    
-    
-    
-    return
+        key = find_key(message, k)
+        print("%21s%3d%5s%6.3f" % ("trying key length:", k, "ic:", v), end="")
+        print(" with key:", key)
+        print("    ", decrypt(message, key))
+        print()
+    print("   ================================\n")
 
+    return
