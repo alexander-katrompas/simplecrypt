fe87f92a25158b448ef1a0037e4f9a58734926e6
alexander-katrompas <alexander.katrompas@austincc.edu>
2020-02-01 19:57:45 -0600

    added testing for vigenere

--- main.py
+++ main.py
@@ -4,31 +4,54 @@ driver file for testing Caesar and Vigenere crypt, decrypt, and cracking
 Author: Alex Katrompas
 """
 
-import constants as cs
 import caesar
+import constants as cs
 import vigenere
 
+CAESAR = False
 
-M = "The entire message."
-K = 'b'
+if CAESAR:
 
-print("Key:", K)
-print("Message:", M)
-print("Length:", len(M), "\n")
+    M = "The entire message for Caesar Cypher."
+    K = 'b'
 
-C = caesar.encrypt(M, cs.offset(K))
-print("Encrypted:", C)
+    print("Key:", K)
+    print("Message:", M)
+    print("Length:", len(M), "\n")
 
-m = caesar.decrypt(C, cs.offset(K))
-print("Decrypted:", M, end=" ")
+    C = caesar.encrypt(M, cs.offset(K))
+    print("Encrypted:", C)
 
-if M == m:
-    print("[success]\n")
+    m = caesar.decrypt(C, cs.offset(K))
+    print("Decrypted:", m, end=" ")
+
+    if M == m:
+        print("[success]\n")
+    else:
+        print("[fail]\n")
+
+    print("Cracking '", C, "' with brute force...\n", sep="")
+    caesar.bruteForceCrack(C)
+
+    print("Cracking '", C, "' with probabilistic cracking...\n", sep="")
+    caesar.probabilisticCrack(C)
+
 else:
-    print("[fail]\n")
+    M = "The entire message for Vigenere Cypher."
+    K = 'vige'
+    
+    print("Key:", K)
+    print("Message:", M)
+    print("Length:", len(M), "\n")
+    
+    C = vigenere.encrypt(M, K)
+    print("Encrypted:", C)
 
-print("Cracking '", C, "' with brute force...\n", sep="")
-caesar.bruteForceCrack(C)
+    m = vigenere.decrypt(C, K)
+    print("Decrypted:", m, end=" ")
 
-print("Cracking '", C, "' with probabilistic cracking...\n", sep="")
-caesar.probabilisticCrack(C)
+    if M == m:
+        print("[success]\n")
+    else:
+        print("[fail]\n")
+
