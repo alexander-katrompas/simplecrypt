44a273f133c70b237fd904e4eb132de97fc65f8e
alexander-katrompas <alexander.katrompas@austincc.edu>
2020-02-01 18:43:27 -0600

    added FREQUENCY, UPPERA, LOWERA, NONALPHA0 added def offset(key)

--- constants.py
+++ constants.py
@@ -1,3 +1,19 @@
 ALPHAUPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALPHALOWER = "abcdefghijklmnopqrstuvwxyz"
 NONALPHA = " !\"#$%&'()*+,-./0123456789:;<=>?@"
+FREQUENCY = "etaoinshrdlcumwfgypbvkjxqz"
+
+UPPERA = ord('A')
+LOWERA = ord('a')
+NONALPHA0 = ord(NONALPHA[0])
+
+def offset(key):
+    off = 0
+    if key in ALPHALOWER:
+        off = ord(key) - LOWERA
+    elif key in ALPHAUPPER:
+        off = ord(key) - UPPERA
+    elif key in NONALPHA:
+        off = ord(key) - NONALPHA0
+    
+    return off
